48ea4b7eea0f3b56d0d5fbb84f25661310345d0b
Alkonosttt <75733469+Alkonosttt@users.noreply.github.com>
2021-05-26 10:29:19 +0300

    Added decryption

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <Windows.h>
 #include <cstring>
 
 using namespace std;
@@ -8,17 +7,39 @@ int main()
 {
     int step;
     char inputText[255];
-    cout << "Enter the text\n";
+    char choice;
+
+    cout << "Enter the text:" << endl;
     cin.getline(inputText, 255);
-    cout << "Enter the key\n";
+    cout << "Enter the key:" << endl;
     cin >> step;
-    for (int i = 0; i < strlen(inputText); i++)
-    {
-        if (step >= 0)
-            inputText[i] = (inputText[i] - 'a' + step) % 26 + 'a';
-        else
-            inputText[i] = (inputText[i] - 'a' + 26 + step) % 26 + 'a';
+
+    cout << "Encryption: type 1"<< endl <<"Decryption: type 2" << endl;
+    cin >> choice;
+    cout << choice << endl;
+
+    if (choice == '1'){
+        cout << "You chose encryption." << endl;
+        for (int i = 0; i < strlen(inputText); i++)
+        {
+            if (step >= 0)
+                inputText[i] = (inputText[i] - 'a' + step) % 26 + 'a';
+            else
+                inputText[i] = (inputText[i] - 'a' + 26 + step) % 26 + 'a';
+        }
+        cout << inputText << endl;
     }
-    cout << inputText << endl;
-    system("pause");
+
+    if (choice == '2'){
+        cout << "You chose decryption." << endl;
+        for (int i = 0; i < strlen(inputText); i++)
+        {
+            if (step >= 0)
+                inputText[i] = (inputText[i] - 'a' + 26 - step) % 26 + 'a';
+            else
+                inputText[i] = (inputText[i] - 'a' - step) % 26 + 'a';
+        }
+        cout << inputText << endl;
+    }
+
 }
